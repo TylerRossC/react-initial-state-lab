47dc125baa9075849ab82b0af28f5735dc292f32
tyler <tylercharawell@gmail.com>
2021-05-26 17:27:19 -0700

    Completed assignment

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,24 @@
-// your Bomb code here!
+import React, {Component} from 'react';
+
+
+export default class Bomb extends Component {
+   
+   constructor(props) {
+       super()
+       this.state = {
+           secondsLeft: props.initialCount
+       }
+   }
+   
+   countdownText = () => {
+       return (this.props.initialCount === 0) ? <h4>Boom!</h4> : <h4>120 seconds left before I go boom!</h4>
+   }
+   
+   render() {
+        return (
+            <div>
+                {this.countdownText()}
+            </div>
+        )
+    }
+}
